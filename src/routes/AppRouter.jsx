a8262b8d7423a0c74887c1cc8c11476b63e1f7bb
Pineda04 <carlosovidiopineda8@gmail.com>
2024-10-23 18:59:39 -0600

    init

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+import { BlogRouter } from "../features/blog/routes"
+import { SecurityRouter } from "../features/security/routes/SecurityRouter"
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        <Route path="/security/*" element={<SecurityRouter />}/>
+        <Route path="/*" element={<BlogRouter />} />
+    </Routes>
+  )
+}
